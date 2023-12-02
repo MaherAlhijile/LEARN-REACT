475b794d3629c4c23c94c99ea0510724696575e8
MaherAlhijile <maher.alhijile@gmail.com>
2023-12-02 21:20:07 +0300

    Final

---
+++ src/components/memberInfo.js
@@ -0,0 +1,8 @@
+const memberInfo =
+[
+    {id:1, img:'/images/01.jpg',    name:'Walter White',    position:'Bryan Cranston',      phone:'+49 12345678',   email:'walter@b-bad.com',   website:'walterwhite.com'   },
+    {id:2, img:'/images/02.jpg',    name:'Jesse Pinkman',   position:'Aaron Paul',          phone:'+49 12345678',   email:'jesse@b-bad.com',    website:'jessepinkman.com'  },
+    {id:3, img:'/images/03.jpg',    name:'Hank Schrader',   position:'Dean Norris',         phone:'+49 12345678',   email:'hank@b-bad.com'                                  },
+]
+
+export default memberInfo
